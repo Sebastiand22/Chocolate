746f91d8e62ed99aa51ec995fda3e84fc0e6bb42
Sebastiand22 <sebastiandunduc16@gmail.com>
2025-04-11 02:21:54 +0300

    products section

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function showMenu() {
-    document.querySelector('.navigation').classList.toggle('active');
-    document.querySelector('.menu').classList.toggle('hide');
-    document.querySelector('.close').classList.toggle('show');
+  document.querySelector(".navigation").classList.toggle("active");
+  document.querySelector(".menu").classList.toggle("hide");
+  document.querySelector(".close").classList.toggle("show");
+}
+
+const modal = document.getElementById("modalProduct");
+
+const btn = document.getElementById("myBtn");
+
+const span = document.getElementsByClassName("close-modal-button")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function (event) {
+   {
+    modal.style.display = "none";
   }
-  
+};
