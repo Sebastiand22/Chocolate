b61ee4fdae73c9654118b6a6da31811891598b1b
Sebastiand22 <sebastiandunduc16@gmail.com>
2025-04-10 22:29:14 +0300

    First commit for this project

---
+++ index.js
@@ -0,0 +1,6 @@
+function showMenu() {
+    document.querySelector('.navigation').classList.toggle('active');
+    document.querySelector('.menu').classList.toggle('hide');
+    document.querySelector('.close').classList.toggle('show');
+  }
+  
